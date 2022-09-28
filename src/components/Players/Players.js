9e0c33b67f5a73ca0c58f6aa7fa85c8601bb9ab6
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-09-28 21:22:04 +0600

    added data on produc.json

---
+++ src/components/Players/Players.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import './Players.css'
+
+const Players = () => {
+  
+  return (
+    <div className='info-container'>
+      <div className="players-container">
+        <h1>Welcome to Players</h1>
+
+      </div>
+      <div className="cart-container">
+        <h1>Welcome to Scores</h1>
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default Players;
