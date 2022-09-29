fef9a3ebaf8b2cdb3cf6ecc2382ab18a3b55ade6
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-09-29 13:13:29 +0600

    added questions and ans

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -71,6 +71,27 @@ const Players = () => {
        <Cart cart={cart}></Cart>
 
       </div>
+      <div>
+        <h1>How does React Works?</h1>
+        <p>React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.</p>
+        
+      <h1>What are the difference between props and state in React?</h1>
+      <p>Simply put, State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.</p>
+
+      <h1>What are the usage of useEffect except api data fething?</h1>
+      <p>Some common use cases of useEffect are:
+        <br />
+
+                  1.Add an event listener for a button
+                  <br />
+                 2. Data fetching from API when component mounts
+                 <br />
+                 3. Perform an action when state or props change
+                 <br />
+                 4. Clean up event listeners when the component unmounts</p>
+      </div>
+      
+ 
       
     </div>
     
